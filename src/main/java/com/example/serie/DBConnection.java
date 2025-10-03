204343fd88f47e2befccac7d3c386720170a9c2f
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-03 11:52:34 -0500

    commit - DBConnection.java :.

--- src/main/java/com/example/serie/DBConnection.java
+++ src/main/java/com/example/serie/DBConnection.java
@@ -1,4 +1,26 @@
 package com.example.serie;
 
+import java.sql.*;
 public class DBConnection {
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    private static final String USER = "system";
+    private static final String PASSWORD = "Tapiero123";
+
+    // metodo para obtener la conexion a oracle :.
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    // metodo para ejecutar el procedimiento almacenado
+    public static void insertarNumero(int numero) {
+        String sql = "{ call insertar_numero(?) }";
+        try (Connection conn = getConnection();
+            CallableStatement stmt = conn.prepareCall(sql)) {
+            stmt.setInt(1, numero);
+            stmt.execute();
+            System.out.println("numero insertado en oracle: "+ numero);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.serie {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.serie to javafx.fxml;
